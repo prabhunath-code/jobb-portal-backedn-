b6aa3bfa9cbc79007a850ee2c2e7eddea044e5ed
prabhu nath <prabhunath@prabhus-MacBook-Air.local>
2025-04-12 20:38:17 +0530

    UPDATE: samesite

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -98,7 +98,7 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpsOnly: true,
-        sameSite: "strict",
+        sameSite: "none",
       })
       .json({
         message: `welcome back ${user.fullname}`,
