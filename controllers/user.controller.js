fd49408d62573f49bcba8d9e8c8dbd1ecaeee454
prabhu nath <prabhunath@prabhus-MacBook-Air.local>
2025-04-12 20:04:10 +0530

    updated: port

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -98,7 +98,6 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpsOnly: true,
-        sameSite: "strict",
       })
       .json({
         message: `welcome back ${user.fullname}`,
